d977a78b27989edea3fb69408ad6a1053f811271
Mariuspersen <marius_kule@hotmail.com>
2025-12-05 14:12:32 +0100

    batman

---
+++ build.zig
@@ -0,0 +1,51 @@
+const std = @import("std");
+const rl = @import("raylib");
+const Config = @import("build.zig.zon");
+
+const opts: rl.Options = .{
+    .raudio = Config.raudio,
+    .rmodels = Config.rmodels,
+    .rshapes = Config.rshapes,
+    .rtext = Config.rtext,
+    .rtextures = Config.rtextures,
+    .platform = Config.platform,
+    .linkage = Config.linkage,
+    .linux_display_backend = Config.linux_display_backend,
+    .opengl_version = Config.opengl_version,
+    .android_ndk = Config.android_ndk,
+    .android_api_version = Config.android_api_version,
+    .config = Config.config,
+};
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+    const raylib_dep = b.dependency("raylib", .{
+        .target = target,
+        .optimize = optimize,
+        .raudio = opts.raudio,
+        .rmodels = opts.rmodels,
+        .rshapes = opts.rshapes,
+        .rtext = opts.rtext,
+        .rtextures = opts.rtextures,
+        .platform = opts.platform,
+        .linkage = opts.linkage,
+        .linux_display_backend = opts.linux_display_backend,
+        .opengl_version = opts.opengl_version,
+        .android_ndk = opts.android_ndk,
+        .android_api_version = opts.android_api_version,
+        .config = opts.config,
+    });
+    const raylib = raylib_dep.artifact("raylib");
+    const root = b.addModule("root", .{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    const exe = b.addExecutable(.{
+        .name = "main",
+        .root_module = root,
+    });
+    exe.linkLibrary(raylib);
+    b.installArtifact(exe);
+}
---
+++ src/main.zig
@@ -0,0 +1,152 @@
+const std = @import("std");
+const time = std.time;
+const epoch = time.epoch;
+
+const raylib = @cImport(
+    @cInclude("raylib.h"),
+);
+
+const raymath = @cImport(
+    @cInclude("raymath.h"),
+);
+
+const green = raylib.struct_Color{
+    .a = 255,
+    .r = 0,
+    .g = 255,
+    .b = 0,
+};
+
+const black = raylib.struct_Color{
+    .a = 0,
+    .r = 0,
+    .g = 0,
+    .b = 0,
+};
+
+const DayStrings = [_][]const u8{
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+};
+
+const MonthStrings = [_][]const u8{
+    "day of the end times",
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+};
+
+pub fn main() !void {
+    raylib.InitWindow(
+        raylib.GetScreenWidth(),
+        raylib.GetScreenHeight(),
+        "Time Kiosk",
+    );
+    defer raylib.CloseWindow();
+
+    raylib.SetTargetFPS(1);
+    raylib.ToggleFullscreen();
+
+    var looping: bool = true;
+
+    const time_fontsize = getMaximumFontSize("55:55:55");
+    const date_fontsize = getMaximumFontSize("Saturday - 35 September");
+    const week_fontsize = getMaximumFontSize("Week 55");
+
+    var buffer: [128:0]u8 = undefined;
+    var writer = std.io.Writer.fixed(&buffer);
+
+    while (!raylib.WindowShouldClose() and looping) {
+        raylib.BeginDrawing();
+        defer raylib.ClearBackground(black);
+        defer raylib.EndDrawing();
+        defer writer.end = 0;
+
+        const key = raylib.GetKeyPressed();
+        defer switch (key) {
+            raylib.KEY_Q => looping = !looping,
+            else => {},
+        };
+
+        const ts = std.time.timestamp();
+        const es = epoch.EpochSeconds{ .secs = @intCast(ts) };
+        const ed = es.getEpochDay();
+        const yd = ed.calculateYearDay();
+        const md = yd.calculateMonthDay();
+        const ds = es.getDaySeconds();
+        const day_of_the_week: usize = @intCast(@mod(@divTrunc(ts, ed.day) + 3, 7));
+        const week_number = @divTrunc(yd.day, 7) + 1;
+
+        try writer.print("{d:02}:{d:02}:{d:02}", .{
+            ds.getHoursIntoDay(),
+            ds.getMinutesIntoHour(),
+            ds.getSecondsIntoMinute(),
+        });
+        try writer.writeByte(0);
+        var measure = raylib.MeasureText(&buffer, time_fontsize);
+        var padding = @divTrunc(raylib.GetScreenWidth() - measure, 2);
+        raylib.DrawText(
+            &buffer,
+            padding,
+            0,
+            time_fontsize,
+            green,
+        );
+
+        writer.end = 0;
+
+        try writer.print("{s} - {d} {s}", .{
+            DayStrings[day_of_the_week],
+            md.day_index,
+            MonthStrings[md.month.numeric()],
+        });
+        try writer.writeByte(0);
+        measure = raylib.MeasureText(&buffer, date_fontsize);
+        padding = @divTrunc(raylib.GetScreenWidth() - measure, 2);
+        raylib.DrawText(
+            &buffer,
+            padding,
+            time_fontsize,
+            date_fontsize,
+            green,
+        );
+
+        writer.end = 0;
+
+        try writer.print("Week {d}", .{week_number});
+        try writer.writeByte(0);
+        measure = raylib.MeasureText(&buffer, week_fontsize);
+        padding = @divTrunc(raylib.GetScreenWidth() - measure, 2);
+        raylib.DrawText(
+            &buffer,
+            padding,
+            time_fontsize + date_fontsize,
+            week_fontsize,
+            green,
+        );
+    }
+}
+
+fn getMaximumFontSize(text: [*c]const u8) i32 {
+    var temp_font_size: i32 = 1;
+    var temp_size: i32 = 0;
+    while (temp_size < raylib.GetScreenWidth()) {
+        temp_size = raylib.MeasureText(text, temp_font_size);
+        temp_font_size += 1;
+    }
+    return temp_font_size - 1;
+}
